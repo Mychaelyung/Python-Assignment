e1d781bbb57f088439ee4a6c5345348105f15f1c
Ajanaku Michael <ajanakuseunmichael@gmail.com>
2025-08-19 20:03:02 +0100

    Assignment 1

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Simple Basic Calculator
+
+# Ask the user for inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+print(f"{num1} + {num2} = {num1 + num2}")
