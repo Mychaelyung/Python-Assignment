88754385870035b401adf1cc83cd0569435bdd97
Ajanaku Michael <ajanakuseunmichael@gmail.com>
2025-08-19 20:04:32 +0100

    Edited

--- calculator.py
+++ calculator.py
@@ -1,6 +1,3 @@
-# Simple Basic Calculator
-
-# Ask the user for inputs
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Enter operation (+, -, *, /): ")
